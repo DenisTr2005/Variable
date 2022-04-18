bbae8df2abf328189cef78c573c7c49cdd09d7a1
DenisTr2005 <den_2005@list.ru>
2022-04-18 18:07:32 +0300

    Main branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var box = 5;
+        System.out.println(box);
+        box+=2;
+        System.out.println(box);
+        box-=3;
+        System.out.println(box);
+        box*=3;
+        System.out.println(box);
+        box/=4;
+        System.out.println(box);
+
+        var liftingCapacity = 50;
+        var stuffWeight = 20;
+        var capacityLeft = liftingCapacity - stuffWeight;
+        System.out.println("Еще можно положить " + capacityLeft + " кг вещей.");
+
+        var appleWeight = 2;
+        var orangesWeight = 3;
+        var fruitWeight = appleWeight + orangesWeight;
+        System.out.println("Общий вес фруктов " + fruitWeight);
+
+        var meatWeight = 4;
+        var waterWeight = 5;
+        var tomatoesWeight = 2;
+        var cucumbersWeight = 2;
+        var peppersWeight = 2;
+        var zucchiniWeight = 2;
+        var vegetablesWeight = tomatoesWeight + cucumbersWeight + peppersWeight + zucchiniWeight;
+        var productsWeight = fruitWeight + vegetablesWeight + meatWeight + waterWeight;
+        System.out.println("Общий вес продуктов " + productsWeight + " кг!");
     }
 }
